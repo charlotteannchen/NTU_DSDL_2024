6608b1a8312c2c999b45187c9d3c14fb54b05bee
charlotteannchen <r12922029@ntu.edu.tw>
2024-04-27 01:40:04 +0800

    Update Lab/LAB-01_(0415)/

--- Lab/LAB-01_(0415)/adders_cla_XOR1.v
+++ Lab/LAB-01_(0415)/adders_cla_XOR1.v
@@ -2,19 +2,19 @@
 `define ADDERS
 `include "gates.v"
 
-// half adder
+// half adder, gate level modeling
 module HA(output C, S, input A, B);
 	XOR g0(S, A, B);
 	AND g1(C, A, B);
 endmodule
 
-// full adder
-module FA(output C, S, input A, B, CI);
+// full adder, gate level modeling
+module FA(output CO, S, input A, B, CI);
 	wire c0, s0, c1, s1;
 	HA ha0(c0, s0, A, B);
 	HA ha1(c1, s1, s0, CI);
 	assign S = s1;
-	OR or0(C, c0, c1);
+	OR or0(CO, c0, c1);
 endmodule
 
 // adder without delay, register-transfer level modeling
@@ -24,29 +24,26 @@ module adder_rtl(
 	input[2:0] A, B, // operands
 	input C0         // carry input
 	);
-	
-	// Implement your code here.
-	// Hint: should be done in 1 line.
-	// You can use this adder to debug the gate-level implemented adder.
-	assign {C3, S} = A + B + C0;
-
+	assign {C3, S} = A+B+C0;
 endmodule
 
 //  ripple-carry adder, gate level modeling
+//  Do not modify the input/output of module
 module rca_gl(
 	output C3,       // carry output
 	output[2:0] S,   // sum
 	input[2:0] A, B, // operands
 	input C0         // carry input
 	);
-	wire[3:0] c;
 
-	assign c[0] = C0;
-	assign C3 = c[3];
-	
-	FA fa0(c[1], S[0], A[0], B[0], c[0]);
-	FA fa1(c[2], S[1], A[1], B[1], c[1]);
-	FA fa2(c[3], S[2], A[2], B[2], c[2]);
+	// TODO:: Implement gate-level RCA
+	wire c1, c2; // internal carry wires
+
+	// Instantiate the Full Adder (FA) modules
+	FA fa0(c1, S[0], A[0], B[0], C0);
+	FA fa1(c2, S[1], A[1], B[1], c1);
+	FA fa2(C3, S[2], A[2], B[2], c2);
+
 endmodule
 
 // carry-lookahead adder, gate level modeling
@@ -64,9 +61,9 @@ module cla_gl(
 	AND g0(G[0],  A[0],  B[0]);
 	AND g1(G[1],  A[1],  B[1]);
 	AND g2(G[2],  A[2],  B[2]);
-	OR p0(P[0], A[0], B[0]);
-	OR p1(P[1], A[1], B[1]);
-	OR p2(P[2], A[2], B[2]);
+	XOR p0(P[0], A[0], B[0]);
+	XOR p1(P[1], A[1], B[1]);
+	XOR p2(P[2], A[2], B[2]);
 
 	assign C[0]=C0;
 	assign C3=C[3];
